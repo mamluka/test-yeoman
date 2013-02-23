25982a1a9ce8eaa1321d316a9b64ffcf7a5cd933
Ray Patterson <ray.patterson@akqa.com>
2013-02-22 17:36:37 -0800

    Done.

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -49,41 +49,30 @@ require.config({
       deps: [ ],
       exports: 'handlebars'
     }
-    
-    // ,
-
-    // 'hbs': {
-    //   deps: [ 'json2' ],
-    //   exports: 'hbs'
-    // }
-
-    // 'hbs': {
-    //   deps: [ 'handlebars', 'i18nprecompile' ]
-    // }
 
   },
 
-  // inlineText: true,
+  inlineText: true,
 
-  // pragmasOnSave: {
-  //   //removes Handlebars.Parser code (used to compile template strings) set
-  //   //it to `false` if you need to parse template strings even after build
-  //   excludeHbsParser : false,
-  //   // kills the entire plugin set once it's built.
-  //   excludeHbs: false,
-  //   // removes i18n precompiler, handlebars and json2
-  //   excludeAfterBuild: false
-  // },
+  pragmasOnSave: {
+    //removes Handlebars.Parser code (used to compile template strings) set
+    //it to `false` if you need to parse template strings even after build
+    excludeHbsParser : true,
+    // kills the entire plugin set once it's built.
+    excludeHbs: true,
+    // removes i18n precompiler, handlebars and json2
+    excludeAfterBuild: true
+  },
 
-  // locale: "en_us",
+  locale: "en_us",
 
-  // // default plugin settings, listing here just as a reference
-  // hbs : {
-  //   templateExtension : 'hbs',
-  //   // if disableI18n is `true` it won't load locales and the i18n helper
-  //   // won't work as well.
-  //   disableI18n : true
-  // },
+  // default plugin settings, listing here just as a reference
+  hbs : {
+    templateExtension : 'hbs',
+    // if disableI18n is `true` it won't load locales and the i18n helper
+    // won't work as well.
+    disableI18n : true
+  },
 
   callback: function(o) {
     try {
