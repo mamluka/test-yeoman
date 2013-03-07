6c2807c76bc1f870c8d3c63517c37d68e6a7c367
Ray Patterson <ray.patterson@akqa.com>
2013-03-06 23:23:55 -0800

    Finally got compadd image helper to work.

--- .compass.rb
+++ .compass.rb
@@ -1,11 +1,2 @@
-images_dir = 'images'
-generated_images_dir = 'images'
-project_path = '.'
-
-http_path = '/'
-
-http_images_path = 'images'
-
-http_generated_images_path = 'images'
-
-relative_assets = true
+http_images_path= '../images' 
+http_generated_images_path = '../images'
